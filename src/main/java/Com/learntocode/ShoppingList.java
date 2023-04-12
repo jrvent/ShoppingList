ac995acf4d19c90e0b921bd12465ec1a17559da6
jrvent <jventura@appdev.yearup.org>
2023-04-12 11:36:06 -0400

    ShoppingList

--- src/main/java/Com/learntocode/ShoppingList.java
+++ src/main/java/Com/learntocode/ShoppingList.java
@@ -2,10 +2,10 @@ package Com.learntocode;
 
 public class ShoppingList {
     public static void main(String[] args) {
-        System.output.println("Steak");
-        System.output.println("Oysters ");
-        System.output.println("Sardines");
-        System.output.println("Mangos");
-        System.output.println("Milk");
-        System.output.println("Chips");
+        System.out.println("Steak");
+        System.out.println("Oysters ");
+        System.out.println("Sardines");
+        System.out.println("Mangos")
+        System.out.println("Milk");
+        System.out.println("Chips");
 }
