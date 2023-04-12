4855ff242deaa7df81af480b13cce4937e41c99c
jrvent <jventura@appdev.yearup.org>
2023-04-12 11:29:07 -0400

    New Maven

---
+++ src/main/java/Com/learntocode/ShoppingList.java
@@ -0,0 +1,11 @@
+package Com.learntocode;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.output.println("Steak");
+        System.output.println("Oysters ");
+        System.output.println("Sardines");
+        System.output.println("Mangos");
+        System.output.println("Milk");
+        System.output.println("Chips");
+}
